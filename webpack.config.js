880c708500bcaf59fa034115d573c854fe508f07
mutoe <mutoe@foxmail.com>
2020-02-14 20:39:39 +0800

    chore: upgrade project to Vue3

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,79 @@
 const path = require('path')
-const HTMLPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/bundle-[hash].js'
-  },
-  devtool: 'cheap-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    hot: true,
-    host: '0.0.0.0',
-    port: 3000,
-    open: false
-  },
-  plugins: [
-    new HTMLPlugin({
-      filename: 'index.html',
-      template: 'src/index.html',
-      inject: 'head'
-    }),
-    new ExtractTextPlugin({
-      filename: 'css/app.css'
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.styl$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            'css-loader',
-            'stylus-loader'
-          ]
-        })
+module.exports = (env = {}) => ({
+    mode: env.prod ? 'production' : 'development',
+    devtool: env.prod ? 'source-map' : 'eval-cheap-module-source-map',
+    entry: path.resolve(__dirname, './src/main.ts'),
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      publicPath: '/dist/',
+    },
+    resolve: {
+      extensions: [ '.ts', '.tsx', '.js', '.jsx' ],
+      alias: {
+        'vue': '@vue/runtime-dom',
       },
-    ]
+    },
+    devServer: {
+      host: '0.0.0.0',
+      port: 3000,
+      inline: true,
+      hot: true,
+      stats: 'minimal',
+      overlay: true,
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          use: 'vue-loader',
+        },
+        {
+          test: /\.tsx?/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                appendTsxSuffixTo: [ /\.vue$/ ],
+                transpileOnly: true,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.png$/,
+          use: {
+            loader: 'url-loader',
+            options: { limit: 8192 },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: { hmr: !env.prod },
+            },
+            'css-loader'
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      }),
+      new HtmlWebpackPlugin({
+        title: 'vue-next-test',
+        template: path.join(__dirname, '/public/index.html'),
+      }),
+    ],
   }
-}
+)
